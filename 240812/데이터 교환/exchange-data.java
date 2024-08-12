c6c1d4cafe9eb24f2b797d42ce7f3b633b06220b
geonsu <97841700+cmsk6405@users.noreply.github.com>
2024-08-12 20:37:51 +0900

    Title: 데이터 교환, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240812/데이터 교환/exchange-data.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp = b;
+        b = a;
+        int temp2 = c;
+        c = temp;
+        a = temp2;
+        System.out.printf("%d\n%d\n%d", a, b, c);
+    }
+}
